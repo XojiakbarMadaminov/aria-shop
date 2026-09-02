cbdc53e2a7f0dd0952aa1e7ced55daf03c1ce875
XojiakbarMadaminov <xmadaminov2202@gmail.com>
2026-09-02 21:31:21 +0500

    print

--- routes/console.php
+++ routes/console.php
@@ -9,5 +9,5 @@ Artisan::command('inspire', function () {
 })->purpose('Display an inspiring quote');
 
 Schedule::command('debtors:send-sms')->dailyAt('9:30')->timezone('Asia/Tashkent');
-Schedule::command('telegram:send-daily-summary')->dailyAt('21:25')->timezone('Asia/Tashkent');
+Schedule::command('telegram:send-daily-summary')->dailyAt('21:33')->timezone('Asia/Tashkent');
 Schedule::command('telegram:send-weekly-summary')->weeklyOn(1, '05:00')->timezone('Asia/Tashkent');
